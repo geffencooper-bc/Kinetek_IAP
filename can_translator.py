11bc6c5f558e9027449d854b7b54f64e5396b6e5
Geffen <geffen.cooper@braincorp.com>
2020-07-06 09:48:33 -0700

    working

--- can_translator.py
+++ can_translator.py
@@ -18,10 +18,10 @@ def switch_frame_id(arg):
         '0x0002' : "CP-->MMC   HOUR_METER_REQUEST            ",
         '0x0005' : "KCH-->MMC  SEND_VERSION_REQUEST_COMMAND  ",  
         '0x03C9' : "UNKNOWN",
-        '0x0040' : "KCH-->MMC  HEART_BEAT\n",
+        '0x0040' : "KCH-->MMC  HEART_BEAT",
         '0x0041' : "KCH-->MMC  HEART_BEAT2",
         '0x0045' : "KCH-->MMC  FW_REVISION_REQUEST",
-        '0x0048' : "KCH-->MMC  IAP_REPLY_TO_LCD",
+        '0x0048' : "KCH-->MMC  IAP_REPLY_TO_LCD (IAP_REQUEST)",
         '0x004F' : "KCH-->MMC  1st 8 bytes",
         '0x0050' : "KCH-->MMC  2nd 8 bytes",
         '0x0051' : "KCH-->MMC  3rd 8 bytes",
@@ -32,8 +32,8 @@ def switch_frame_id(arg):
         '0x0056' : "?-->?      FW_UNKNOWN",
         '0x0060' : "?-->?      NOT_USED",
         '0x0067' : "MMC-->KCH  FW_REVISION_RESPONSE",
-        '0x0069' : "MMC-->KCH  HOST_IAP_REQUEST",
-        '0x0080' : "MMC-->CP   **HEART_BEAT**\n",
+        '0x0069' : "MMC-->KCH  HOST_IAP_REQUEST (IAP_RESPONSE)",
+        '0x0080' : "MMC-->CP   **HEART_BEAT**",
         '0x0081' : "MMC-->CP   CONTROLLER_CHANGE_VERIFICATION",
         '0x0082' : "MMC-->CP   HOUR_METER_RESPONSE",
         '0x04CA' : "?-->?      CONTROL_PANEL_UPDATE_COMMAND"
@@ -77,15 +77,10 @@ def switch_state(arg):
         '00 02' : "REVERSE"
     }.get(arg)
 
-# def switch_IAP_ID(arg):
-#     return{
-#         '80 00 00 00 00' : "CONTROLLER_ENTER_IAP_MODE"
-
-#     }.get(arg)
 
 def switch_IAP_data(arg):
     return{
-        '03 27' : "\tREPROGRAM_CONFIRM",
+        '03 27' : "\tEnter IAP Mode",
         '10 10 10 10 10 10 10 10' : "\treceived 32 bytes",
         '88 88 88 88 88 88 88 88' : "\tstart sending bytes request",
         '99 99 99 99 99 99 99 99' : "\tready to receive bytes response",
@@ -104,10 +99,10 @@ def switch_IAP_data(arg):
 
 
 # open the csv file and create the csv reader object
-with open('/home/geffen.cooper/vm_shared/can_logs/SC2.27-2.28_full.csv', 'r') as csv_file:
+with open('/home/geffen.cooper/vm_shared/can_logs/SC2.27-2.28_full_02.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
 
-    translated_data = "\t\t\t\t\----------START OF STREAM----------\n\n"
+    translated_data = ""# "\t\t\t\t\----------START OF STREAM----------\n\n"
 
     # parse through all rows in csv file
     for row in reader:
@@ -128,6 +123,6 @@ with open('/home/geffen.cooper/vm_shared/can_logs/SC2.27-2.28_full.csv', 'r') as
         
         # parse the KT heart beat frames
         if frm_id == "0x0080":
-             translated_data +=  "page: " + str(row[DATA])[6:8] + '\n' # + "\t" + str(switch_hb_data(str(row[DATA])[10:]))
+             translated_data +=  "page: " + str(row[DATA])[6:8] # + "\t" + str(switch_hb_data(str(row[DATA])[10:]))
 
     print(translated_data)
