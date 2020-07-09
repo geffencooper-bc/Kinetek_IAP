771ca07cbb7446c102a7f676bae1bdd8eb1d7827
Geffen <geffen.cooper@braincorp.com>
2020-07-09 16:41:10 -0700

    fixed regex bug

--- ota_scripts/IAPUtil.py
+++ ota_scripts/IAPUtil.py
@@ -60,22 +60,22 @@ class IAPUtil:
         self.bus = can.interface.Bus(bustype='socketcan', channel='can0')
 
     def put_in_IAP_mode(self):
-        self.bus.send(ENTER_IAP_MODE_REQUEST)
-        start_time = frame.timestamp
+        self.bus.send(self.ENTER_IAP_MODE_REQUEST)
+        #start_time = frame.timestamp
         print("Message sent on {}".format(self.bus.channel_info))   
         resp = self.bus.recv(timeout=1000)
         #curr_time = resp.timestamp
         enter_iap_mode_resp_count = 0
         while enter_iap_mode_resp_count < 2:
             if decode_socketcan_packet(resp) != ENTER_IAP_MODE_RESPONSE:
-                self.bus.send(ENTER_IAP_MODE_REQUEST) 
+                self.bus.send(self.ENTER_IAP_MODE_REQUEST) 
                 #print("SENT:\t", frame)  
                 resp = self.bus.recv(timeout=1000)
                 print("RECEIVED:\t", resp)
                 #curr_time = resp.timestamp
                 if resp.arbitration_id == 0x80:
-                     print("HEART BEAT DETECTED, DIDNT ENTER")
-                     break
+                     print("=========HEART BEAT DETECTED, DIDNT ENTER")
+                     return False
             else:
                 enter_iap_mode_resp_count += 1
         print("RECEIVED:\t", resp)
@@ -86,17 +86,19 @@ class IAPUtil:
         resp = self.bus.recv(timeout=1000)
         print("RECEIVED:\t", resp)
         resp = self.bus.recv(timeout=1000)
-        print("entered iap_mode")
+        return True
+        #print("entered iap_mode")
 
-    def check_if_in_iap_mode(self):
+    def check_if_in_iap_mode(self): # if sent fw rev req it becomes 060 | 81 00 00 00 00 00 00 00
         timeout_count = 0 # if receive 20 messages with <2 iap_mode signifiers, not in IAP mode
+        enter_iap_mode_resp_count = 0
         while enter_iap_mode_resp_count < 2:
             resp = self.bus.recv(timeout=1000)
-            if count > 20:
+            if timeout_count > 20:
                 return False
             if decode_socketcan_packet(resp) != ENTER_IAP_MODE_RESPONSE:   
                 resp = self.bus.recv(timeout=1000)
-                count += 1
+                timeout_count += 1
             else:
                 enter_iap_mode_resp_count += 1
         return True
@@ -106,6 +108,7 @@ class IAPUtil:
     def send_request(self, request, expected_response, timeout_count):
         self.bus.send(request)  
         resp = self.bus.recv(timeout=1000)
+        print("RECEIVED:\t", resp)
         count = 0
         while lookup(decode_socketcan_packet(resp), IAP_data_lookup) != expected_response:
             resp = self.bus.recv(timeout=1000)
@@ -116,15 +119,69 @@ class IAPUtil:
         return True
 
     def send_init_packets(self):
+        print("sending init packets")
         # make sure in iap mode first
         if self.check_if_in_iap_mode() == True:
             # request fw revision
             if self.send_request(self.FW_REVISION_REQUEST, "FW_REVISION_REQUEST_RESPONSE", 10) == False:
-                return "CONTROLLER WONT RECEIVE BYTES"
+                return "CONTROLLER WONT RESPOND TO FW REVISION REQUEST"
             # request to send bytes
             if self.send_request(self.SEND_BYTES_REQUEST, "SEND_BYTES_RESPONSE", 10) == False:
                 return "CONTROLLER WONT RECEIVE BYTES"
-            #if self.send_request()
+            # request to send start address
+            if self.send_request(self.SEND_START_ADDRESS_REQUEST, "SEND_START_ADDRESS_RESPONSE", 10) == False:
+                return "CONTROLLER WONT RECEIVE START ADDRESS"
+            # request to send checksum data
+            if self.send_request(self.SEND_CHECKSUM_DATA_REQUEST, "SEND_CHECKSUM_DATA_RESPONSE", 10) == False:
+                return "CONTROLLER WONT RECEIVE CHECKSUM DATA"
+            # request to send start address
+            if self.send_request(self.SEND_DATA_SIZE_REQUEST, "SEND_DATA_SIZE_RESPONSE", 10) == False:
+                return "CONTROLLER WONT RECEIVE DATA SIZE"
+            return "SUCCESS"
+        return "NOT IN IAP"
+    
+    def upload_image(self):
+        print("sending hex file...")
+        current_packet = [] # store in case need to retry
+        write_ids = [0x04F, 0x050, 0x051, 0x052]     
+        write_ids_retry = [0x053, 0x054, 0x055, 0x056]
+        
+        while True:
+            status = self.send_hex_packet(write_ids)
+            while status == True: # keep sending hex packets until status false or none
+                status = self.send_hex_packet(write_ids)
+            if status == None: # reached end of file
+                return True
+            if status == False: # retry
+                status = self.send_hex_packet(write_ids_retry)
+                if status == False:
+                    return False
+                continue
+            
+            
+
+    def send_hex_packet(write_ids): #32 bytes of data
+        i = 0
+        while True:
+            data = hexUtil.get_next_data_8() # get the next 8 data bytes from the hex file
+            self.current_packet.append(data) # stores these in case need to retry
+            if data == -1: # means eof so break loop
+                self.upload_done = True
+                return None
+            
+            hex_frame = make_socketcan_packet(write_ids[i],data) # make a socket_can packet from hex data
+            if i == 3: # if this is the fourth packet, ait for 32 bytes confirmation from Kinetek
+                if self.send_request(hex_frame, "RECEIVED_32__BYTES") == False: # if no confirmation, return false
+                    return False
+                self.current_packet.clear # if receive confirmation can clear and return true
+                return True
+
+            else: # if first three packets then send normally
+                self.bus.send(request) 
+
+            i += 1
+            if i == len(write_ids):
+                i = 0
 
        
 
@@ -142,7 +199,7 @@ class IAPUtil:
 #         # self.curr_time_stamp = time_stamp
 
 def decode_socketcan_packet(frame):
-    can_id = hex(frame.arbitration_id)[2:].zfill(3) # form: "060"
+    can_id = hex(int(frame.arbitration_id,16))[2:].zfill(3) # form: "060"
     data = ""
     for byte in frame.data:
         data += hex(byte)[2:].zfill(2).upper() + " " # form: "00 00 00 00 00 00 00 00"
@@ -153,6 +210,10 @@ ut = IAPUtil()
 ut.load_hex_file("2.28_copy.hex")
 ut.to_string()
 
-#ut.init_can()
-#ut.put_in_IAP_mode()
-#ut.send_init_packets()
+ut.init_can()
+ut.put_in_IAP_mode()
+# if ut.put_in_IAP_mode() == True:
+#     print("sending packets")
+#     time.sleep(3)
+#     ut.send_init_packets()
+print(ut.send_init_packets())
--- ota_scripts/KinetekCodes.py
+++ ota_scripts/KinetekCodes.py
@@ -1,5 +1,5 @@
 from HexUtility import make_socketcan_packet, data_string_to_byte_list
-
+import re
 # FW_REVISION_REQUEST = make_socketcan_packet(0x045, data_string_to_byte_list("00 00 00 00 00 00 00 00"))
 # ENTER_IAP_MODE_REQUEST = make_socketcan_packet(0x048, data_string_to_byte_list("00 00 00 00 00 00 00 00"))
 # SEND_BYTES_REQUEST = make_socketcan_packet(0x048, data_string_to_byte_list("88 88 88 88 88 88 88 88"))
@@ -8,22 +8,22 @@ ENTER_IAP_MODE_RESPONSE = "060 | 80 00 00 00 00"
 
 # used for decoding requests/responses
 IAP_data_lookup = [
-
-    ('069 | 10 10 10 10 10 10 10 10' ,                                                            "RECEIVED_32__BYTES"),
-    ('069 | 99 99 99 99 99 99 99 99' ,                                                            "SEND_BYTES_RESPONSE"),
-    ('067 | [0-9A-F][0-9A-F] [0-9A-F][0-9A-F] 5E|5F [0-9A-F][0-9A-F] [0-9A-F][0-9A-F] 00 00 00' , "FW_REVISION_REQUEST_RESPONSE"),
-    ('069 | 02 10 10 10 10 10 10 10' ,                                                            "SEND_CODE_START_ADDRESS_RESPONSE"),
-    ('069 | 03 10 10 10 10 10 10 10' ,                                                            "SEND_CODE_CHECKSUM_DATA_RESPONSE"),
-    ('069 | 04 10 10 10 10 10 10 10' ,                                                            "SEND_CODE_DATA_SIZE_RESPONSE"),
-    ('069 | 05 20 20 20 20 20 20 20' ,                                                            "CALCULATE_CHECKSUM_RESPONSE"),
-    ('069 | 07 40 40 40 40 40 40 40' ,                                                            "CALCULATE_PAGE_CHECKSUM_RESPONSE"),
-    ('84 [0-9A-F][0-9A-F] [0-9A-F][0-9A-F] [0-9A-F][0-9A-F] [0-9A-F][0-9A-F]',                    "calculated page checksum")
+ 
+    ('069\s\|\s10\s10\s10\s10\s10\s10\s10\s10' ,                                                                    "RECEIVED_32__BYTES"),
+    ('069\s\|\s99\s99\s99\s99\s99\s99\s99\s99' ,                                                                    "SEND_BYTES_RESPONSE"),
+    ('067\s\|\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s5E|5F\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s00\s00\s00' , "FW_REVISION_REQUEST_RESPONSE"),
+    ('069\s\|\s02\s10\s10\s10\s10\s10\s10\s10' ,                                                                    "SEND_START_ADDRESS_RESPONSE"),
+    ('069\s\|\s03\s10\s10\s10\s10\s10\s10\s10' ,                                                                     "SEND_CHECKSUM_DATA_RESPONSE"),
+    ('069\s\|\s04\s10\s10\s10\s10\s10\s10\s10' ,                                                                    "SEND_DATA_SIZE_RESPONSE"),
+    ('069\s\|\s05\s20\s20\s20\s20\s20\s20\s20' ,                                                                    "CALCULATE_CHECKSUM_RESPONSE"),
+    ('069\s\|\s07\s40\s40\s40\s40\s40\s40\s40' ,                                                                    "CALCULATE_PAGE_CHECKSUM_RESPONSE"),
+    ('060\s\|\s84\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]\s[0-9A-F]',                 "calculated page checksum")
 ] 
 
 # find the according pattern in the above table
-def lookup(self,data, table):
+def lookup(data, table):
     for pattern, value in table:
-        if re.search(pattern, data):
+        if re.match(pattern, data):
             return value
     return ""
 
