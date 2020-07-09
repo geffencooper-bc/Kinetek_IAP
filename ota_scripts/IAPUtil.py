72956b7ff1cc86ad512087f136a1e9d84da7cc48
Geffen <geffen.cooper@braincorp.com>
2020-07-09 09:57:35 -0700

    iap mode works

--- ota_scripts/IAPUtil.py
+++ ota_scripts/IAPUtil.py
@@ -1,7 +1,7 @@
 # class to automate iap process like get code size, get start address, send hex file
 from HexUtility import *
 from KinetekCodes import *
-
+import time
 class IAPUtil:
     def __init__(self):
         self.code_size_bytes = 0
@@ -26,29 +26,67 @@ class IAPUtil:
     def init_can(self):
         # implement later
         self.bus = can.interface.Bus(bustype='socketcan', channel='can0')
-        pass
 
     def put_in_IAP_mode(self):
-        command = make_socketcan_packet(get_kinetek_can_id_code("IAP_REQUEST"), get_kinetek_data_code("ENTER_IAP_MODE"))
-        # while loop
+        frame = ENTER_IAP_MODE_REQUEST
+        self.bus.send(frame)
+        start_time = frame.timestamp
+        print("Message sent on {}".format(self.bus.channel_info))   
+        resp = self.bus.recv(timeout=1000)
+        curr_time = resp.timestamp
+        enter_iap_mode_resp_count = 0
+        while enter_iap_mode_resp_count < 2:
+            if decode_socketcan_packet(resp) != ENTER_IAP_MODE_RESPONSE:
+                #frame = ENTER_IAP_MODE_REQUEST
+                self.bus.send(frame)
+                #print("Message sent on {}".format(self.bus.channel_info)) 
+                #print("SENT:\t", frame)
+                #print("Message sent on {}".format(self.bus.channel_info))   
+                resp = self.bus.recv(timeout=1000)
+                print("RECEIVED:\t", resp)
+                #curr_time = resp.timestamp
+                if resp.arbitration_id == 0x80:
+                     print("HEART BEAT DETECTED, DIDNT ENTER")
+                     break
+            else:
+                enter_iap_mode_resp_count += 1
+        print("entered iap_mode")
+        # count = 0
+        # while count < 121:
+        #     frame = ENTER_IAP_MODE_REQUEST
+        #     self.bus.send(frame)
+        #     count += 1
+        
+        # count = 0
+        # while count < 1000:
+        #     resp = self.bus.recv(timeout=1000)
+        #     print(resp)
+        #     count += 1
 
 
-class SimpleFrame:
-    last_time_stamp = 0
-    curr_time_stamp = 0
-    time_stamp_delta = 0 # used to detect a timeout
-    def __init__(self, time_stamp, can_id, data):
-        self.time_stamp = time_stamp
-        self.can_id = can_id
-        self.data = data
+# class SimpleFrame:
+#     # last_time_stamp = 0
+#     # curr_time_stamp = 0 # these are static variables shared accross all instances
+#     # time_stamp_delta = 0 # used to detect a timeout
+#     def __init__(self, time_stamp, can_id, data):
+#         self.time_stamp = time_stamp
+#         self.can_id = can_id
+#         self.data = data
+#         # update these
+#         # self.last_time_stamp = self.curr_time_stamp
+#         # self.curr_time_stamp = time_stamp
 
 def decode_socketcan_packet(frame):
-    can_id = hex(frame.arbitration_id)[2:].zfill(3)
-        data = ""
-        for byte in frame.data:
-           data += hex(byte)[2:].zfill(2).upper() + " "
-        simple_frame = SimpleFrame(frame.timestamp, can_id, data[:-1])
+    can_id = hex(frame.arbitration_id)[2:].zfill(3) # form: "060"
+    data = ""
+    for byte in frame.data:
+        data += hex(byte)[2:].zfill(2).upper() + " " # form: "00 00 00 00 00 00 00 00"
+    #simple_frame = SimpleFrame(frame.timestamp, can_id, data[:-1])
+    return str(can_id + " | " + data)
 
 ut = IAPUtil()
 ut.load_hex_file("2.28_copy.hex")
 ut.to_string()
+
+ut.init_can()
+ut.put_in_IAP_mode()
--- ota_scripts/sample.py
+++ ota_scripts/sample.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import can 
 bus = can.interface.Bus(bustype='socketcan', channel='can0')
-msg = can.Message(arbitration_id=0x048,
-                    data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+msg = can.Message(arbitration_id=0x001,
+                    data=[0x1D, 0xF7, 0x05, 0x00, 0x00],
                     is_extended_id=False)
 print(msg)
 
@@ -11,9 +11,15 @@ try:
     print("Message sent on {}".format(bus.channel_info))   
     msg = bus.recv(timeout=1000)
     print(msg)
-    while msg.arbitration_id != 0x60:
-        msg = bus.recv(timeout=1000)
-        print(msg)
+    msg = bus.recv(timeout=1000)
+    print(msg)
+    msg = bus.recv(timeout=1000)
+    print(msg)
+    msg = bus.recv(timeout=1000)
+    print(msg)
+    # while msg.arbitration_id != 0x60:
+    #     msg = bus.recv(timeout=1000)
+    #     print(msg)
     # data = ""
     # for byte in msg.data:
     #     data += hex(byte)[2:].zfill(2).upper() + " "
