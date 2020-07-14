09e63c989eb7ab5df5411e95cb07711339cc923d
Geffen <geffen.cooper@braincorp.com>
2020-07-14 12:38:23 -0700

    can read from can bus using c/c++ sokcetcan

--- C++_version/Main.cpp
+++ C++_version/Main.cpp
@@ -1,6 +1,56 @@
-#include "TestFunc.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/can.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <linux/if.h>
+#include <sys/ioctl.h>
 
 int main()
 {
-    testFunc();
+    int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+
+    s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+
+    strcpy(ifr.ifr_name, "can0" );
+    ioctl(s, SIOCGIFINDEX, &ifr);
+
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+
+    bind(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    struct can_frame frame;
+
+    while(true)
+    {
+        int nbytes = read(s, &frame, sizeof(struct can_frame));
+
+        if (nbytes < 0) {
+                perror("can raw socket read");
+                return 1;
+        }
+
+        /* paranoid check ... */
+        if (nbytes < sizeof(struct can_frame)) {
+                fprintf(stderr, "read: incomplete CAN frame\n");
+                return 1;
+        }
+
+        // printf("Received a CAN frame from interface %s", ifr.ifr_name);
+        printf("\ncan_id: %02X\t", frame.can_id);
+        printf("data:\t");
+        for(int i = 0; i < frame.can_dlc; i++)
+        {
+            printf("%02X ", frame.data[i]);
+        }
+    }
+    
 }
